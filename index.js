0191131295545f659039ab1ab1431750dc356a6c
Duke <hnguyen1179@berkeley.edu>
2021-08-06 15:48:46 -0700

    working on generating individual review scores based on total score

--- index.js
+++ index.js
@@ -1,6 +1,7 @@
 require("dotenv").config({ path: __dirname + "/.denv" });
 const fs = require("fs");
 const puppeteer = require("puppeteer");
+const { v4: uuidv4 } = require("uuid");
 
 const LA_URL =
 	"https://www.airbnb.com/s/Los-Angeles--CA--United-States/homes?tab_id=home_tab&refinement_paths%5B%5D=%2Fhomes&flexible_trip_dates%5B%5D=august&flexible_trip_dates%5B%5D=september&flexible_trip_lengths%5B%5D=weekend_trip&date_picker_type=flexible_dates&search_type=user_map_move&ne_lat=34.28128357811729&ne_lng=-117.85726835397077&sw_lat=33.63200856318738&sw_lng=-118.81170560983014&zoom=10&search_by_map=true&place_id=ChIJE9on3F3HwoAR9AhGJW_fL-I";
@@ -36,6 +37,7 @@ const LA_URL =
 // Can iterate through the elements by listing list and then extract information per listing per iteration
 
 const LISTINGS_COUNT = 15;
+
 /**
  * This function grabs:
  *  1. Type of Listing
@@ -52,12 +54,10 @@ const LISTINGS_COUNT = 15;
  */
 
 const scrollDown = () => {
-	const scrollable = document.querySelector("._8rtpcxs ._3j8fry");
+	const scrollable = document.querySelector("._1v5ksyp");
 
 	const heightToScroll =
 		document.querySelector("._8rtpcxs ._3j8fry").offsetHeight;
-
-	console.log(heightToScroll);
 
 	scrollable.scrollTo({
 		left: 0,
@@ -175,6 +175,15 @@ const detailedListingExtraction = () => {
 		})
 		?.filter((x) => x.length > 2);
 
+	const scores = [
+		...document.querySelectorAll(".ciubx2o.dir.dir-ltr ._1s11ltsf"),
+	]?.map((x) => {
+		const score = x.querySelector("._4oybiu").innerText;
+		const label = x.querySelector("._y1ba89").innerText;
+
+		return [label, score];
+	});
+
 	return {
 		city,
 		highlights,
@@ -188,12 +197,67 @@ const detailedListingExtraction = () => {
 		hostDetails,
 		houseRules,
 		healthAndSafety,
+		scores,
 	};
 };
 
-const reviewsExtraction = () => {
-	return [...document.querySelectorAll("._1gjypya")]?.map(
-		(x) => [...x.children][1].innerText
+const extractReviews = async (page, id) => {
+	const moreReviews = await page.evaluate(() =>
+		document.querySelector(".b1sec48q.v7aged4.dir.dir-ltr")
+	);
+
+	let query = ".rici162.dir.dir-ltr";
+
+	if (moreReviews !== null) {
+		query = "._1gjypya";
+
+		await Promise.all([
+			page.click(".b1sec48q.v7aged4.dir.dir-ltr"),
+			page.waitForNavigation({
+				waitUntil: "networkidle2",
+			}),
+			page.waitForTimeout(1000),
+		]);
+
+		const scrollFactor = +(Math.random() * 5 + 2).toFixed(0);
+
+		for (let i = 0; i < scrollFactor; i++) {
+			await Promise.all([
+				page.evaluate(scrollDown),
+				page.waitForTimeout(500),
+			]);
+		}
+	}
+
+	return page.evaluate(
+		({ query, listingId }) =>
+			[...document.querySelectorAll(query)].map((x) => {
+				const generateRandDate = (dateString) => {
+					const date = new Date(dateString);
+					const numDays = new Date(
+						date.getFullYear(),
+						date.getMonth(),
+						0
+					).getDate();
+
+					return new Date(
+						date.getFullYear(),
+						date.getMonth(),
+						Math.ceil(Math.random() * numDays)
+					).toString();
+				};
+
+				const date = x
+					.querySelector("._1ixuu7m")
+					.innerText.split("\n,")[0];
+
+				return {
+					listingId,
+					date: generateRandDate(date),
+					content: x.children[1].innerText,
+				};
+			}),
+		{ query, listingId: id }
 	);
 };
 
@@ -230,41 +294,38 @@ const scraperMain = async (browser, page) => {
 				detailedListingExtraction
 			);
 
+			detailedListing.id = uuidv4();
+
 			// Go into reviews and extract some users & reviews
-			const moreReviews = await listingPage.evaluate(() =>
-				document.querySelector(".b1sec48q.v7aged4.dir.dir-ltr")
+			const listingReviews = await extractReviews(
+				listingPage,
+				detailedListing.id
 			);
-
-			let listingReviews;
-
-			if (moreReviews !== null) {
-				await Promise.all([
-					listingPage.click(".b1sec48q.v7aged4.dir.dir-ltr"),
-					listingPage.waitForNavigation({
-						waitUntil: "networkidle2",
-					}),
-					listingPage.waitForTimeout(1000),
-				]);
-
-				await Promise.all([
-					listingPage.evaluate(scrollDown),
-					listingPage.waitForTimeout(500),
-				]);
-
-				listingReviews = await listingPage.evaluate(reviewsExtraction);
-			} else {
-				listingReviews = await listingPage.evaluate(() =>
-					[...document.querySelectorAll(".rici162.dir.dir-ltr")].map(
-						(x) => x.children[1].innerText
-					)
-				);
-			}
 
 			listingPage.close();
 
-			console.log(detailedListing);
-			console.log(basicListing);
-			console.log(listingReviews);
+			console.log(detailedListing); // Object
+			console.log(basicListing);    // Object
+			console.log(listingReviews);  // Array of Objects
+
+			// TODO :
+			// ✓ 1. Generate a proper date from "July 2021" => "July 3, 2021UTC:000", etc.An actual DateTime type with a random, valid day set.
+			// This value will go into the 'createdAt' column in our Review Model;
+			// From this, we can generate a random, valid reservation based on our review
+			// new Date('July 3, 2021'); Just need to find how many days are in that specific month and create a random day that isn't too close to the end of the month; cuz ez
+
+			// 2. Find the total score of a listing; create fake individual review scores that then
+			// will need to average out to match the total score of the listing
+
+			// ✓ 3. Generated a unique ID for each listing
+
+			// 4. Extract SVGs and Images from each listing
+
+			// 5. Push out a MASSIVE object for each listing that combines all the necessary
+			// information for proper models of LISTING, HOST, REVIEWS (PARTIAL)
+			// USERS and RESERVATIONS will be generated via JS on upon seeding based on the REVIEWS (PARTIAL)
+			// USERS -> Creates REVIEWS (FULL) & PREV & FUTURE RESERVATIONS
+			// HOSTS -> Created LISTINGS
 
 			// output.push(basicListing);
 		}
@@ -334,6 +395,7 @@ const scraperMain = async (browser, page) => {
 		verified					Boolean
 
 	NEW LISTING MODEL
+		id								Int						//User Generated via UUID
 		title							String
 		street						String				//Generated
 		city							String
@@ -351,9 +413,10 @@ const scraperMain = async (browser, page) => {
 		listingType				String
 		basicAmenities		String[] 
 		amenities					String[]
-		houseRules				String[] 			// ex. ['Check-in: After 3:00 PM', 'Checkout: 11:00 AM', 'Self Check In', ''No Smoking', 'No pets', 'No parties']
-		healthAndSafety 	String[] 			// ex. ['Committed to Airbnb's enhanced cleaning process', 'Airbnb's social-distancing and other COVID-19...'...]
-		highlights				String[][] 		// ex. [['Entire Home', 'You'll have the place to yourself'], ['x','y'], ]
+		houseRules				String[] 			// ex. ['Check-in: After 3:00 PM', 'Checkout: 11:00 AM', 'Self Check In', ''No Smoking', 'No pets', 'No parties', ...]
+		healthAndSafety 	String[] 			// ex. ['Committed to Airbnb's enhanced cleaning process', 'Airbnb's social-distancing and other COVID-19...', ...]
+		highlights				String[][] 		// ex. [['Entire Home', 'You'll have the place to yourself'], ['x','y'], ...]
+		scores						String[][]		// ex. [['Cleanliness', '5.0'], ['Location', '3.9'], ...]				
  */
 
 // CITY
