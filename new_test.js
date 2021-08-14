8bc92d77d97bd32e75c268d7340e3617d13d05b9
Duke <hnguyen1179@berkeley.edu>
2021-08-13 18:49:48 -0700

    finalized data

--- new_test.js
+++ new_test.js
@@ -2,4 +2,4 @@ const resDist = require("./future_res_dist.json");
 const users = require("./user_and_reviews_reservations.json");
 const hosts = require("./host_and_listings.json");
 
-console.log(users[11].reservations)
+console.log(users)
